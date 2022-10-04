c92ea1ae4dd273a284758d7a6b1329e22f015691
Paulo Marques Gonçalves <82685447+PauloMarquesG@users.noreply.github.com>
2022-10-03 22:00:27 -0300

    habilidades
    ajuste no menu de habilidades

--- js/script.js
+++ js/script.js
@@ -42,38 +42,48 @@ $(document).ready(function() {
 function tabHabilidadeChecked(){
   if (document.getElementById("tab-front-end").checked) {
     document.getElementById("front-end").style.display = "block";
-    document.getElementById("btn-front-end").classList.add("active");
+    document.getElementById("link-front-end").classList.add("active");
+    document.getElementById("link-front-end").classList.add("border-bottom");
   }else {
     document.getElementById("front-end").style.display = "none";
-    document.getElementById("btn-front-end").classList.remove("active");
+    document.getElementById("link-front-end").classList.remove("active");
+    document.getElementById("link-front-end").classList.remove("border-bottom");
   }
   if (document.getElementById("tab-back-end").checked) {
     document.getElementById("back-end").style.display = "block";
-    document.getElementById("btn-back-end").classList.add("active");
+    document.getElementById("link-back-end").classList.add("active");
+    document.getElementById("link-back-end").classList.add("border-bottom");
   }else {
     document.getElementById("back-end").style.display = "none";
-    document.getElementById("btn-back-end").classList.remove("active");
+    document.getElementById("link-back-end").classList.remove("active");
+    document.getElementById("link-back-end").classList.remove("border-bottom");
   }
   if (document.getElementById("tab-data-base").checked) {
     document.getElementById("data-base").style.display = "block";
-    document.getElementById("btn-data-base").classList.add("active");
+    document.getElementById("link-data-base").classList.add("active");
+    document.getElementById("link-data-base").classList.add("border-bottom");
   }else {
     document.getElementById("data-base").style.display = "none";
-    document.getElementById("btn-data-base").classList.remove("active");
+    document.getElementById("link-data-base").classList.remove("active");
+    document.getElementById("link-data-base").classList.remove("border-bottom");
   }
   if(document.getElementById("tab-ferramenta").checked) {
     document.getElementById("ferramenta").style.display = "block";
-    document.getElementById("btn-ferramenta").classList.add("active");
+    document.getElementById("link-ferramenta").classList.add("active");
+    document.getElementById("link-ferramenta").classList.add("border-bottom");
   }else {
     document.getElementById("ferramenta").style.display = "none";
-    document.getElementById("btn-ferramenta").classList.remove("active");
+    document.getElementById("link-ferramenta").classList.remove("active");
+    document.getElementById("link-ferramenta").classList.remove("border-bottom");
   }
   if(document.getElementById("tab-idioma").checked) {
     document.getElementById("idioma").style.display = "block";
-    document.getElementById("btn-idioma").classList.add("active");
+    document.getElementById("link-idioma").classList.add("active");
+    document.getElementById("link-idioma").classList.add("border-bottom");
   }else {
     document.getElementById("idioma").style.display = "none";
-    document.getElementById("btn-idioma").classList.remove("active");
+    document.getElementById("link-idioma").classList.remove("active");
+    document.getElementById("link-idioma").classList.remove("border-bottom");
   }
 }
 
